2db10172a9895a99320efcf9724cc65e7b80acb2
DonielMoins <theshark556@gmail.com>
2021-05-27 23:47:12 -0400

    Support for python 3.10

--- utils/requests.py
+++ utils/requests.py
@@ -1,41 +1,64 @@
 from typing import Optional
+import re 
 import requests
 import classes.requestObj as ReqObj
+import logging
 
 # NOTE USE IN THREAD FROM THREADPOOL OR ELSE BLOCKING
 # make single request checking what type from template
-def makeRequest(requestTemplate: ReqObj, data=None, session=Optional[requests.Session]):
+def _makeRequest(requestTemplate: ReqObj, data: Optional[list[str]], session: requests.Session):
     reqtype = requestTemplate.reqtype
     
-    URL = requestTemplate.uri
+    
+    URL = _parseLink(requestTemplate.uri, data)
     request = requests.Request(str(reqtype).upper(), URL)
     
-    # TODO: Using python 3.10 we can use PEP 634 (match and case) but too bad its a pain in the ass to upgrade.
-    if reqtype == "get":
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
-    elif reqtype == "head":
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
-    elif reqtype == "post":
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
-    elif reqtype == "patch":
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
-    elif reqtype == "put":
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
-    elif reqtype == "delete":
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
-    else:
-        # OPTIONS REQUEST
-        prepedreq = session.prepare_request()
-        response: requests.Response = session.send(prepedreq)
+    match requestTemplate.reqtype:
+        case "request":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "get":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "head":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "post":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "patch":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "put":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "delete":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case "options":
+            prepedreq = session.prepare_request()
+            response: requests.Response = session.send(prepedreq)
+        case _:
+            logging.warn(f"Unknown request method {str(requestTemplate.reqtype)} in {repr(requestTemplate)}")
+            logging.debug("How did this request get past checks")
     
-def MakeRequests(requestList: list, data=None, session=requests.Session()):
+def MakeRequests(requestList: list, dataList: Optional[list[list[str]]], session=requests.Session()):
     for request in requestList:
         request: ReqObj.Request = request
-        makeRequest(request, data=data, session=session)
+        if dataList is not None:
+            for data in dataList:
+                _makeRequest(request, data, session)
+        else:
+            _makeRequest(request, None, session)
+
+def _parseLink(uri: str, data: Optional[list[str]]):
+    finalURI = uri
+    if data is not None:
+        matches = re.findall("/{([0-9])+}/g", uri)
+        if len(matches) <= len(data):
+            for index, match in enumerate(matches):
+                finalURI.replace(match, data[index])
+        else:
+            logging.warn("Not enough input present.")
+    return finalURI
         
